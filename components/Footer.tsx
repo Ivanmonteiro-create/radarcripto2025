4c427c34d1df3b4f9294f1a262a1d0cd632af343
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-17 19:57:43 +0100

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "14px 20px", opacity: 0.8 }}>
+      <small>
+        © {new Date().getFullYear()} RadarCrypto • Fase 1 •
+        {" "}versão <code>cac199b00e85327d86f6011253c6963c45d5b79c</code>
+      </small>
+    </footer>
+  );
+}
