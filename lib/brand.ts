7c9db0466d5b2c4e3afafb0328cac7570f39c4bf
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-25 13:02:58 +0100

    Create brand.ts

---
+++ lib/brand.ts
@@ -0,0 +1,7 @@
+// lib/brand.ts
+export const BRAND_NAME = 'RadarCripto';
+export const BRAND_DOMAIN = 'radarcripto.space';
+export const BRAND_URL = `https://${BRAND_DOMAIN}`;
+
+// opcional: manter o alias em SEO (não mostrar na UI)
+export const BRAND_ALT_NAMES = ['RadarCrypto'];
