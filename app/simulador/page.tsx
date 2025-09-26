9e7b33571c26585b7b9c33e8af2dbd284a3fb611
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-26 11:19:29 +0100

    Update page.tsx

--- app/simulador/page.tsx
+++ app/simulador/page.tsx
@@ -1,17 +1,10 @@
 // app/simulador/page.tsx
-import TradingViewWidget from '@/components/TradingViewWidget';
-import TradeControls from '@/components/TradeControls';
+import SimPageClient from './SimPageClient';
 
 export default function SimuladorPage() {
   return (
-    <main className="wrapper">
-      <section className="panel" style={{ gridColumn: '1 / span 2', minHeight: 520 }}>
-        <TradingViewWidget symbol="BINANCE:BTCUSDT" />
-      </section>
-
-      <section className="panel compactPanel">
-        <TradeControls />
-      </section>
+    <main>
+      <SimPageClient />
     </main>
   );
 }
