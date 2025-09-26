1f140da3e7205c636e10655ef5874ec0cc9d6176
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-26 12:06:22 +0100

    Update page.tsx

--- app/simulador/page.tsx
+++ app/simulador/page.tsx
@@ -1,8 +1,4 @@
-// app/simulador/page.tsx
-import dynamic from "next/dynamic";
-
-// Carrega o cliente sem SSR (TradingView precisa de window)
-const SimPageClient = dynamic(() => import("./SimPageClient"), { ssr: false });
+import SimPageClient from "./SimPageClient";
 
 export default function Page() {
   return <SimPageClient />;
