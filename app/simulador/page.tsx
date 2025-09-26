eedec7bbc3d8a0dfad64caeb6ce8ceb864ac649a
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-26 11:40:32 +0100

    Update page.tsx

--- app/simulador/page.tsx
+++ app/simulador/page.tsx
@@ -1,8 +1,4 @@
-// app/simulador/page.tsx
-import dynamic from "next/dynamic";
-
-// Renderiza só no cliente (o widget do TradingView precisa de window)
-const SimPageClient = dynamic(() => import("./SimPageClient"), { ssr: false });
+import SimPageClient from "./SimPageClient";
 
 export default function Page() {
   return <SimPageClient />;
