a95b4d37f05cf9731a1b6748355a4c4a97cdc87e
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-26 14:40:22 +0100

    Update page.tsx

--- app/simulador/page.tsx
+++ app/simulador/page.tsx
@@ -1,19 +1,6 @@
-// app/simulador/page.tsx
-import Link from "next/link";
-import SimPageClient from "./SimPageClient";
-
-export const metadata = {
-  title: "Simulador | RadarCrypto",
-};
+// /app/simulador/page.tsx
+import SimPageClient from './SimPageClient';
 
 export default function Page() {
-  // A page é “fina”: só orquestra. Nada de dynamic/ssr:false aqui.
-  return (
-    <main className="aboutV2-root" style={{ paddingTop: 12 }}>
-      <Link href="/" className="btn backTopRight tcBackBtn">
-        Voltar ao início
-      </Link>
-      <SimPageClient />
-    </main>
-  );
+  return <SimPageClient />;
 }
