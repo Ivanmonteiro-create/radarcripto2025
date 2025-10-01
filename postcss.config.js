ba7a8ab7b21eab029fcd2ae22865285e7107e9fa
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-10-01 02:27:53 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
-export default {
+// postcss.config.js  (CommonJS — o que o Next espera)
+module.exports = {
   plugins: {
+    // Se você usa classes utilitárias do Tailwind, deixe a linha abaixo.
+    // Se NÃO usa Tailwind, remova a linha 'tailwindcss: {}' (deixe só autoprefixer).
     tailwindcss: {},
     autoprefixer: {},
   },
