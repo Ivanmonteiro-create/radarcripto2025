9bd83714a580bbd6ecc00581d28dacc7e47454a4
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-10-07 01:36:53 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "RadarCrypto — Fase 1",
