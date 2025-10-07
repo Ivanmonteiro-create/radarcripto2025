080d5d76782173010357c4dc6ae3c653012f99fd
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-10-07 01:04:21 +0100

    Update page.tsx

--- app/simulador/page.tsx
+++ app/simulador/page.tsx
@@ -1,6 +1,23 @@
 // /app/simulador/page.tsx
 import SimPageClient from './SimPageClient';
+// app/simulador/page.tsx
+import Link from "next/link";
 
+export default function SimuladorPage() {
+  return (
+    <section className="container section">
+      {/* Barra de título + botão à direita */}
+      <header className="rc-pagebar">
+        <h1 className="rc-pagebar__title">Controles de Trade</h1>
+        <Link href="/" className="rc-btn rc-btn--green">Voltar ao início</Link>
+      </header>
+
+      {/* ---- seu conteúdo atual do simulador abaixo ---- */}
+      {/* <Grafico /> */}
+      {/* <TradeControls /> */}
+    </section>
+  );
+}
 export default function Page() {
   return <SimPageClient />;
 }
